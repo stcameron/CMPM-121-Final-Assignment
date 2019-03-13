7299715198e875378d675de8e2eb5e11835e10aa
Steven Cameron <stcamero@ucsc.edu>
2019-03-13 03:17:26 -0700

    Character and resource gathering

---
+++ Final Project/Assets/Scenes/Scripts/PickupScroll.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PickupScroll : MonoBehaviour
+{
+
+    //public GameObject pickupEffect;
+    public Text countText;
+    private int count;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        count = 0;
+        SetCountText();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    void OnTriggerEnter(Collider other)
+
+    {
+
+        if (other.gameObject.CompareTag("Scroll"))
+        {
+            //Instantiate(pickupEffect, other.transform.position, other.transform.rotation);
+            Destroy(other.gameObject);
+            count += 1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Scroll: " + count.ToString();
+    }
+
+}
